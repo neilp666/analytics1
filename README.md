ea0268b902b0ad9564806b6be76326a186e87d5b
neil patel <neil@myindianweddingapp.com>
2014-05-22 15:32:06 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+== README
+
+Updated version of the analytics demo
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Analytics1::Application.config.secret_key_base = '87938a6216ff084061ae0bbea4ebe6503429d4e14a2ca614b2c0d8a951a8d00e2451f3ab65aecb0534f6da913e8d4da09c47bdefda1ae129e0f0e9a0913bf0c0'
